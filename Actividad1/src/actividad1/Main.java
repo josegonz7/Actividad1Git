e9be312e3421b24054282adf0d0931ca28c54637
josegonz7 <jgoncre1412@g.educaand.es>
2026-02-06 12:09:40 +0100

    ejericio definitivo

--- Actividad1/src/actividad1/Main.java
+++ Actividad1/src/actividad1/Main.java
@@ -10,7 +10,7 @@ public class Main {
  System.out.println("Resta: " + calc.restar(a, b));
  // === BLOQUE A (solo Alumno A) ===
  Saludo s = new Saludo();
- System.out.println(s.saludar("DAW1"));
+ System.out.println(s.saludar("DAW"));
 
  // === BLOQUE B (solo Alumno B) ===
  }
