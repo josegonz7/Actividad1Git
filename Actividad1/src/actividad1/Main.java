2d9c596aa8fe819e3952f5ffc77b6782e7da4192
josegonz7 <jgoncre1412@g.educaand.es>
2026-02-06 12:05:27 +0100

    ejercicio alumno A

---
+++ Actividad1/src/actividad1/Main.java
@@ -0,0 +1,17 @@
+package actividad1;
+
+public class Main {
+ public static void main(String[] args) {
+ Calculadora calc = new Calculadora();
+ int a = 10, b = 3;
+ String etiqueta = "BASE";
+ System.out.println("Etiqueta: " + etiqueta);
+ System.out.println("Suma: " + calc.sumar(a, b));
+ System.out.println("Resta: " + calc.restar(a, b));
+ // === BLOQUE A (solo Alumno A) ===
+ Saludo s = new Saludo();
+ System.out.println(s.saludar("DAW"));
+
+ // === BLOQUE B (solo Alumno B) ===
+ }
+}
---
+++ Actividad1/src/actividad1/Saludo.java
@@ -0,0 +1,7 @@
+package actividad1;
+
+public class Saludo {
+	public String saludar(String nombre) {
+		 return "Hola, " + nombre + "!";
+	}
+}
